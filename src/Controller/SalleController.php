109198cc9b5cd137a960ec23d3926412657a30e8
victor-sd <victor.savary-derelle@etud.u-picardie.fr>
2020-03-03 12:26:04 +0100

    commit entity

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -2,22 +2,63 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Salle;
 
 
 class SalleController extends AbstractController{
- public function accueil() {
-
+public function accueil() {
     $nombre = rand(1,84);
     return $this->render('salle/accueil.html.twig',
-        array('numero' => $nombre)); // ou ['numero'=>$nombre]);
- //return new Response("ici l'accueil !");
- /*return new Response("<html><body><h1>Salles :</h1> <p>Voici quelques
-informations concernant les salles<br \>blablabla blablabla blablabla blablabla
-blablabla blablabla blablabla blablabla ....</p></body></html>"); */
+    array('numero' => $nombre)); // ou ['numero'=>$nombre]);
 }
 
 public function afficher($numero) {
+    if ($numero > 50)
+    throw $this->createNotFoundException('C\'est trop !');
+    else
     return $this->render('salle/afficher.html.twig',
-    array( 'numero' => $numero ));
+    array('numero' => $numero)); 
     }
+
+public function dix() {
+    return $this->redirectToRoute('salle_tp_afficher', array('numero' => 10));
+} 
+
+public function testXml(Request $request) {
+    $remoteAddr = $request->server->get('REMOTE_ADDR');
+    $rep = new Response;
+    $rep->setContent('<?xml version="1.0" encoding="UTF-8"?><remoteAddr>'
+    .$remoteAddr.'</remoteAddr>');
+    $rep->headers->set('Content-Type', 'text/xml');
+    return $rep;
+    }
+
+public function testJson(Request $request) {
+    $remoteAddr = $request->server->get('REMOTE_ADDR'); 
+    $data = array('remoteAddr' => $remoteAddr);
+    return new JsonResponse($data);
+} 
+
+public function treize() {
+    $salle = new Salle;
+    $salle->setBatiment('D');
+    $salle->setEtage(1);
+    $salle->setNumero(13);
+    return $this->render('salle/treize.html.twig',
+    array('salle' => $salle));
+    }
+
+public function quatorze() {
+    $salle = new Salle;
+    $salle->setBatiment('D');
+    $salle->setEtage(1);
+    $salle->setNumero(13);
+    return $this->render('salle/quatorze.html.twig',
+    array('designation' => $salle->__toString()));
+    //ou seulement $salle
+}
+       
+
 }
---
+++ src/Entity/Salle.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SalleRepository")
+ */
+class Salle
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $batiment;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $etage;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $numero;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBatiment(): ?string
+    {
+        return $this->batiment;
+    }
+
+    public function setBatiment(string $batiment): self
+    {
+        $this->batiment = $batiment;
+
+        return $this;
+    }
+
+    public function getEtage(): ?int
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(int $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->getBatiment().'-'.$this->getEtage().'.'.$this->getNumero();
+    }
+       
+}
