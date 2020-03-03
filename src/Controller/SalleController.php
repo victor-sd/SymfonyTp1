b389389ab3ee065d0abbd2d2d77c1281b3c241e8
victor-sd <victor.savary-derelle@etud.u-picardie.fr>
2020-03-03 10:14:11 +0100

    chap1 page4

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -1,11 +1,23 @@
 <?php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
-class SalleController {
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class SalleController extends AbstractController{
  public function accueil() {
+
+    $nombre = rand(1,84);
+    return $this->render('salle/accueil.html.twig',
+        array('numero' => $nombre)); // ou ['numero'=>$nombre]);
  //return new Response("ici l'accueil !");
- return new Response("<html><body><h1>Salles :</h1> <p>Voici quelques
+ /*return new Response("<html><body><h1>Salles :</h1> <p>Voici quelques
 informations concernant les salles<br \>blablabla blablabla blablabla blablabla
-blablabla blablabla blablabla blablabla ....</p></body></html>"); 
- }
+blablabla blablabla blablabla blablabla ....</p></body></html>"); */
+}
+
+public function afficher($numero) {
+    return $this->render('salle/afficher.html.twig',
+    array( 'numero' => $numero ));
+    }
 }
